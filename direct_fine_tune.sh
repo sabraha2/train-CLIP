9c7edf9e2fdfa43a62bef8808596075dc3b2db5d
Sophia Abraham <sabraha2@nd.edu>
2024-03-29 02:15:09 -0400

    add train scripts

---
+++ direct_fine_tune.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#$ -M sabraha2@nd.edu
+#$ -m abe
+#$ -q gpu-long
+#$ -l gpu_card=1
+#$ -l h=!qa-a10-*
+#$ -e errors/
+#$ -N direct-clip-fine-tune
+
+
+module load python
+module load intel
+module load cuda
+module load cudnn
+
+source /afs/crc.nd.edu/user/s/sabraha2/Projects/SPIE_2024/venv/bin/activate
+
+python train_finetune.py --folder /project01/cvrl/sabraha2/DSIAC_CLIP_DATA/ --batch_size 32 --num_workers 32 --default_root_dir /project01/cvrl/sabraha2/SPIE_2024/direct_fine_tune/
